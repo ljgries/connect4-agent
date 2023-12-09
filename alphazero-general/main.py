44f5e4aaeda3fcfc3fc9763149742bcca9c49149
Ritvik Singh <ritvik.singh@vanderbilt.edu>
2023-12-08 18:02:37 -0600

    Change alphazero general defaults

--- alphazero-general/main.py
+++ alphazero-general/main.py
@@ -12,7 +12,7 @@ log = logging.getLogger(__name__)
 coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
 
 args = dotdict({
-    'numIters': 1000,
+    'numIters': 250,
     'numEps': 100,              # Number of complete self-play games to simulate during a new iteration.
     'tempThreshold': 15,        #
     'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
@@ -23,7 +23,7 @@ args = dotdict({
 
     'checkpoint': './temp/',
     'load_model': False,
-    'load_folder_file': ('./models/connect4','best.pth.tar'),
+    'load_folder_file': ('./models/','best.pth.tar'),
     'numItersForTrainExamplesHistory': 20,
 
 })
